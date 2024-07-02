c5e28ee65859edc8dad3c65c9a29f1e4b5e02f13
NicolasFuentes AziendeGlobal <nicolas.fuentes@aziende.global>
2024-07-02 16:01:20 -0400

    FILTROS

--- app/Crud/template_view_list_filters.php
+++ app/Crud/template_view_list_filters.php
@@ -2,7 +2,7 @@
     <label for="%OBJETO_LABEL_INDIVIDUAL%" class=" form-label">%OBJETO_LABEL%</label>
     <select class="form-control form-select form-select-transparent" aria-label="Select example" id="%OBJETO_LABEL_INDIVIDUAL%" name="%OBJETO_LABEL_INDIVIDUAL%">
         <option value="">---</option>
-        @foreach($%OBJETO_LABEL% as $%OBJETO_LABEL_INDIVIDUAL%Option)
+        @foreach($%OBJETO_LABEL%List as $%OBJETO_LABEL_INDIVIDUAL%Option)
         <option value="{{ $%OBJETO_LABEL_INDIVIDUAL%Option->%FIELD_ID% }}">{{ $%OBJETO_LABEL_INDIVIDUAL%Option->%FIELD_NAME% }}</option>
         @endforeach
     </select>
