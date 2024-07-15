50d73bb27d2e3303a8ece2c1ec7950b8af6beae0
NicolasFuentes AziendeGlobal <nicolas.fuentes@aziende.global>
2024-07-15 05:20:25 -0400

    contactos4

--- routes/web_crud.php
+++ routes/web_crud.php
@@ -4,3 +4,12 @@
 
 
         
+
+        use App\Http\Controllers\Crud\Contactos4Controller;
+        Route::name('crm.')->group(function () {
+            Route::resource('/crm/Contactos4', Contactos4Controller::class);
+        }); 
+        Route::get('crm/Contactos4DataTable',[Contactos4Controller::class, 'getContactos4DataTable'])->name('crm.Contactos4DataTable');
+        Route::get('crm/Contactos4DataTable/create',[Contactos4Controller ::class, 'create'])->name('Contactos4DataTable.create');
+
+                
