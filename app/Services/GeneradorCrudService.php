9ff3a7d73f8c8a4c02255045dccbe578dcba67ad
NicolasFuentes AziendeGlobal <nicolas.fuentes@aziende.global>
2024-08-01 08:58:50 -0400

    accordion

--- app/Services/GeneradorCrudService.php
+++ app/Services/GeneradorCrudService.php
@@ -663,7 +663,9 @@ class GeneradorCrudService
 
                 $value = '
                     @foreach($' . $model_name . ' as $item)
-                    <option value="{{ $item->' . $column_id . ' }}"  {{ (isset($' . $data['crud_name'] . ') && $item->' . $column_id . ' == $' . $data['crud_name'] . '->' . $show_column_name . ')?"selected":"" }}>{{ $item->' . $column_name . ' }}</option>
+                    <option value="{{ $item->' . $column_id . ' }}"  {{ (isset($' . $data['crud_name'] . ') && $item->' . $column_id . ' == $' . $data['crud_name'] . '->' . $show_column_name . ')?"selected":"" }} {{ (request()->has("' . $column_id . '") && $item->' . $column_id . ' == request()->get("' . $column_id . '")) ? "selected" : "" }} >
+                    {{ $item->' . $column_name . ' }}
+                    </option>
                     @endforeach';
 
                 $template = str_replace('%FIELD_SELECT_OPTIONS%', $value, $template);
