1751c4fee18f9411a46b619ff1227aab27c0ea97
NicolasFuentes AziendeGlobal <nicolas.fuentes@aziende.global>
2024-09-19 18:21:15 -0400

    search

--- app/Crud/template_view_list_filters_form_text.php
+++ app/Crud/template_view_list_filters_form_text.php
@@ -1,6 +1,8 @@
            
-<div class="col-12 col-lg-4">
-    <label for="texto" class=" form-label">Buscar</label>
-    <input type="text" class="form-control form-control-transparent" id="texto" name="texto" value="{{ (isset($texto))?$texto:'' }}" />
+<!--begin::Search-->
+<div class="d-flex align-items-center position-relative my-1">
+    {!! getIcon('magnifier', 'fs-3 position-absolute ms-5') !!}
+    <input type="text" data-kt-crud-table-filter="search" class="form-control form-control-solid w-250px ps-13" placeholder="Buscar" id="texto" name="texto" value="{{ (isset($texto))?$texto:'' }}" />
 </div>
+<!--end::Search-->
 
