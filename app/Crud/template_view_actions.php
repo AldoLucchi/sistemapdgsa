ffb8705f5144709bac423ace8b5a1205a26ae307
NicolasFuentes AziendeGlobal <nicolas.fuentes@aziende.global>
2024-10-10 20:01:29 -0400

    accordion - edit

--- app/Crud/template_view_actions.php
+++ app/Crud/template_view_actions.php
@@ -1,6 +1,17 @@
  
 
     <!--begin::Menu item-->
+    <div class="menu-item px-3 %OBJETO_UPDATE%">
+        @if( (request()->segment(2) == '%OBJETO%') || (request()->segment(2) != '%OBJETO%' && Session::has('%OBJETO%_update') ) )
+        <!--<a href="#" class="menu-link px-3" data-kt-%OBJETO_ROUTE%-id="{{ $%OBJETO_VARIABLE%->%FIELD_ID% }}" data-bs-toggle="modal" data-bs-target="#kt_modal_add_%OBJETO_ROUTE%" data-kt-action="update_row">-->
+        <a href="{{ url('/'.(Session::has('%OBJETO_ROUTE%')?Session::get('%OBJETO_ROUTE%'):'crud/%OBJETO_ROUTE%').'/'. $%OBJETO_VARIABLE%->%FIELD_ID% .'/edit' ) }}{{ (!in_array('%OBJETO_VARIABLE%',request()->segments()))? ('?redirect_url='.url()->previous()) : ''}}" class="menu-link px-3">
+        <i class="fa fa-edit fs-4"></i>
+        </a>
+        @endif
+    </div>
+    <!--end::Menu item-->
+    
+    <!--begin::Menu item-->
     <div class="menu-item px-3 %OBJETO_DELETE%">
         @if( (request()->segment(2) == '%OBJETO%') || (request()->segment(2) != '%OBJETO%' && Session::has('%OBJETO%_delete') ) )
         <form id="destroy_%OBJETO_ROUTE%_form_{{ $%OBJETO_ROUTE%->%FIELD_ID% }}" name="destroy_%OBJETO_ROUTE%_form_{{ $%OBJETO_ROUTE%->%FIELD_ID% }}" class="form destroy_%OBJETO_ROUTE%_form" action="{{ route('crud.%OBJETO_ROUTE%.destroy', $%OBJETO_ROUTE%) }}" method="POST">
@@ -11,7 +22,6 @@
             </button>
         </form>
         @endif
-
     </div>
     <!--end::Menu item-->
 
