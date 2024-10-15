668ac66e3905bb702273066a2efd0b8b5149cbda
NicolasFuentes AziendeGlobal <nicolas.fuentes@aziende.global>
2024-10-15 08:03:36 -0400

    buscador

--- app/Crud/template_view_list_filters_form_text.php
+++ app/Crud/template_view_list_filters_form_text.php
@@ -1,7 +1,7 @@
            
 <!--begin::Search-->
 <div class="d-flex align-items-center position-relative my-1">    
-    <input type="text" data-kt-crud-table-filter="search" class="form-control form-control-solid w-250px ps-13" placeholder="Buscar" id="texto" name="texto" value="{{ (isset($texto))?$texto:'' }}" />
+    <input type="text" data-kt-crud-table-filter="search" class="form-control form-control-solid w-250px ps-3" placeholder="Buscar" id="texto" name="texto" value="{{ (isset($texto))?$texto:'' }}" />
     <span id="texto_search" name="texto_search" style="cursor:pointer;">{!! getIcon('magnifier', 'fs-3  ms-1 ') !!}</span>
 </div>
 <!--end::Search-->
