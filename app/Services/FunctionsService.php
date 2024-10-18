de2db1c06054476c1c07cdea8217374d459e199e
NicolasFuentes AziendeGlobal <nicolas.fuentes@aziende.global>
2024-10-18 09:55:27 -0400

    archivos fix

--- app/Services/FunctionsService.php
+++ app/Services/FunctionsService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class FunctionsService
 {
@@ -11,9 +12,17 @@ class FunctionsService
     public function getCustomFilename($recurso, $originalName, $urlAmigable)
     {
         $prefijo = strval(Carbon::now()->hour) . strval(Carbon::now()->minute) . strval(Carbon::now()->second);
+        Log::info('FunctionsService - getCustomFilename - ' . $originalName);
         $nameOriginal = str_replace(" ", "", $originalName);
         $nameOriginal = $this->filename_sanitizer($nameOriginal);
+        Log::info('FunctionsService - getCustomFilename - ' . $nameOriginal);
+
+        $nameOriginalArray = explode('.', $nameOriginal);
+        $extensionOriginal = end($nameOriginalArray);
         $nameOriginal = substr($nameOriginal, 0, 15);
+        $nameOriginal = $nameOriginal . '.' . $extensionOriginal;
+        Log::info('FunctionsService - getCustomFilename - ' . $nameOriginal);
+
         $filename = $recurso . '_' . env('APP_ENV') . '_' . str_replace(" ", "", $urlAmigable) . '_' . $prefijo . '_' . $nameOriginal;
         return $filename;
     }
